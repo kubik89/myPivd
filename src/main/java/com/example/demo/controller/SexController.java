c3ecad9e5e23c36bd01eae0486e7ea511e4bb5db
Volodymyr <kubik89@yahoo.com>
2021-04-19 17:34:05 +0300

    added Crossorigin for SexController

--- src/main/java/com/example/demo/controller/SexController.java
+++ src/main/java/com/example/demo/controller/SexController.java
@@ -3,10 +3,12 @@ package com.example.demo.controller;
 import com.example.demo.dto.SexDtoList;
 import com.example.demo.service.ISexService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RequestMapping(value = "/sex")
 @RestController
 public class SexController {
